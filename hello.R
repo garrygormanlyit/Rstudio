883e5ef64d25fe802b6abc3939954d72bdb064ca
Garry Gorman <l00150690@student.lyit.ie>
2022-03-10 11:07:37 +0000

    commit

---
+++ hello.R
@@ -0,0 +1,16 @@
+x <- 7+7
+print(x)
+x
+y <- x - 3
+y
+y <- 2
+y
+z <- c(1, 1, 9, 3.14)
+z <- c(z, 555, z)
+z <- z * 2 + 100 #multiplys each element by 2 and adds 100 to each element
+z
+my_sqrt <- sqrt(z - 1)
+my_sqrt
+
+my_data <- c(1, 2, 3, 4) + c(0, 10)
+my_data
